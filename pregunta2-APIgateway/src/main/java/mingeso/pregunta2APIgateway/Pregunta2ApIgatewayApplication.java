53ee47965e133d7044c7dcd636df1cb6b745a6bc
Estefania Alvarez <estefania.alvarez@usach.cl>
2022-07-06 04:30:58 -0400

    tuvo weno

---
+++ pregunta2-APIgateway/src/main/java/mingeso/pregunta2APIgateway/Pregunta2ApIgatewayApplication.java
@@ -0,0 +1,13 @@
+package mingeso.pregunta2APIgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pregunta2ApIgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pregunta2ApIgatewayApplication.class, args);
+	}
+
+}
---
+++ pregunta2-APIgateway/src/test/java/mingeso/pregunta2APIgateway/Pregunta2ApIgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mingeso.pregunta2APIgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pregunta2ApIgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
