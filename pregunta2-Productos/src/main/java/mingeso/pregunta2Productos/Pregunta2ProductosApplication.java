e5ea108f03f7919c13a3c932d10c436ed3ab7b2a
Estefania Alvarez <estefania.alvarez@usach.cl>
2022-07-06 00:45:25 -0400

    xd 2

--- pregunta2-Productos/src/main/java/mingeso/pregunta2Productos/Pregunta2ProductosApplication.java
+++ pregunta2-Productos/src/main/java/mingeso/pregunta2Productos/Pregunta2ProductosApplication.java
@@ -5,7 +5,9 @@ import mingeso.pregunta2Productos.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
